f530de983f2e41e16ac6e64943ffbdee94285bc1
Roman Bolshiyanov <bolshiyanov@gmail.com>
2021-01-28 09:56:24 +0000

    added commits for ru

--- plugins/confirm.js
+++ plugins/confirm.js
@@ -1,8 +1,11 @@
+// Это плагин confirm, а именно модальное окно с удалением фрукта.
 // Так как модальное окно может висеть сколько угодно долго, 
 // имеет смысл обрабатывать его асинхронно. Для этого создаем 
-// компонен Confirm.js внутри компонента modalBody.js и используем Promise.
+// компонен Confirm.js внутри компонента index.js и используем Promise.
 
 $.confirm = function (options) {
+    // Промис $.confirm позволяет выполняться всему коду приложения, пока отрисовано модальное окно 
+    // confirm и не нажата ни одна кнопка, продолжается выполнение программы.
     return new Promise((resolve, reject) => {
         const modal = $.modal({
             title: options.title,
@@ -10,25 +13,23 @@ $.confirm = function (options) {
             closable: false,
             content: options.content,
             // Так как при каждом закрытии окна у нас остается модальное окно в dom,
-            // что приводит к утечке памяти, будем использовать Hook onClose 
-            // для удаления Vmodal из Dom дерева, который был реализован в modal.js:
+            // что приводит к утечке памяти, будем использовать Hook onClose который (был создан в modal.js)
+            // для удаления Vmodal из Dom дерева, который был реализован в modal.js :
+            // когда then передает true из 
             onClose() {
                 modal.destroy()
             },
+           // Конец реализации хука тут
 
             footerButtons: [
-                {
-                    text: 'Отменить', type: 'secondary', handler() {
+                {text: 'Отменить', type: 'secondary', handler() {
                         modal.close()
                         reject() 
-                    }
-                },
-                {
-                    text: 'Удалить', type: 'danger', handler() {
+                    }},
+                {text: 'Удалить', type: 'danger', handler() {
                         modal.close()
                         resolve()
-                    }
-                }
+                    }}
             ]
         })
 
