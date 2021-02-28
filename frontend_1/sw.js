5a427f260e2a329257de69315ae9efb43abbd994
bolshiyanov <bolshiyanov@gmail.com>
2021-02-28 14:49:07 +0000

    pwa test v2.1.7

--- frontend_1/sw.js
+++ frontend_1/sw.js
@@ -24,13 +24,13 @@ const assets = [
   'images/sleep.jpg',
   'images/medisilogo.png',
   'favicon.png',
-  'meta/favicon-16x16.png',
-  'meta/favicon-32x32.png',
-  'meta/favicon-96x96.png',
-  'medisiaudio_12.mp3',
-  'medisiaudio_12.webm',
-  'medisiaudio_13.webm',
-  'medisiaudio_13.webm'
+  // 'meta/favicon-16x16.png',
+  // 'meta/favicon-32x32.png',
+  // 'meta/favicon-96x96.png',
+  // 'medisiaudio_12.mp3',
+  // 'medisiaudio_12.webm',
+  // 'medisiaudio_13.webm',
+  // 'medisiaudio_13.webm'
 ];
 
 // cache size limit function
@@ -46,7 +46,7 @@ const limitCacheSize = (name, size) => {
 
 // install event
 self.addEventListener('install', evt => {
-  //console.log('service worker installed');
+  console.log('service worker installed');
   evt.waitUntil(
     caches.open(staticCacheName).then((cache) => {
       console.log('caching shell assets');
@@ -57,10 +57,10 @@ self.addEventListener('install', evt => {
 
 // activate event
 self.addEventListener('activate', evt => {
-  //console.log('service worker activated');
+  console.log('service worker activated');
   evt.waitUntil(
     caches.keys().then(keys => {
-      //console.log(keys);
+      console.log(keys);
       return Promise.all(keys
         .filter(key => key !== staticCacheName && key !== dynamicCacheName)
         .map(key => caches.delete(key))
