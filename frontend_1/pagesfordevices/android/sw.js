ff590798615d2790b26ff72096d312dd63ee883c
bolshiyanov <bolshiyanov@gmail.com>
2021-03-02 09:47:35 +0000

    v.2.4.1 Добавляем кнопку PWA для андроида

--- frontend_1/pagesfordevices/android/sw.js
+++ frontend_1/pagesfordevices/android/sw.js
@@ -1,89 +1,36 @@
-const staticCacheName = 'site-static-v4';
-const dynamicCacheName = 'site-dynamic-v4';
-const assets = [
-  '/',
-  'index.html',
-  'app.js',
-  '../../plugins/core.js',
-  '../../plugins/siriwave.js',
-  '../../plugins/player1.js',
-  '../../plugins/player2.js',
-  '../../plugins/player3.js',
-  '../../plugins/player4.js',
-  'linkCopy.js',
-  '../../css/index.css',
-  '../../css/styles.css',
-  'player1.html',
-  'player2.html',
-  'player3.html',
-  'player4.html',
-  'fallback.html',
-  'logo50.png',
-  '../../images/abundance.jpg',
-  '../../images/health.jpg',
-  '../../images/relationships.jpg',
-  '../../images/sleep.jpg',
-  '../../images/medisilogo.png',
-  'favicon.png',
-  '../../meta/favicon-16x16.png',
-  '../../meta/favicon-32x32.png',
-  '../../meta/favicon-96x96.png'
+// This is the service worker with the Cache-first network
+
+const CACHE = "pwabuilder-precachev2";
+const precacheFiles = [
+  /* Add an array of files to precache for your app */
+  
 ];
 
-// cache size limit function
-const limitCacheSize = (name, size) => {
-  caches.open(name).then(cache => {
-    cache.keys().then(keys => {
-      if(keys.length > size){
-        cache.delete(keys[0]).then(limitCacheSize(name, size));
-      }
-    });
-  });
-};
+self.addEventListener("install", function (event) {
+  console.log("[PWA Builder] Install Event processing");
 
-// install event
-self.addEventListener('install', evt => {
-  console.log('service worker installed');
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
+  console.log("[PWA Builder] Skip waiting on install");
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
     })
   );
 });
 
-// activate event
-self.addEventListener('activate', evt => {
-  console.log('service worker activated');
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      console.log(keys);
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
+// Allow sw to control of current page
+self.addEventListener("activate", function (event) {
+  console.log("[PWA Builder] Claiming clients for current page");
+  event.waitUntil(self.clients.claim());
 });
 
-// fetch events
-self.addEventListener('fetch', evt => {
-  if(evt.request.url.indexOf('admin@cluster0.milmt.mongodb.net') === -1){
-    evt.respondWith(
-      caches.match(evt.request).then(cacheRes => {
-        return cacheRes || fetch(evt.request).then(fetchRes => {
-          return caches.open(dynamicCacheName).then(cache => {
-            cache.put(evt.request.url, fetchRes.clone());
-            // check cached items size
-            limitCacheSize(dynamicCacheName, 2);
-            return fetchRes;
-          })
-        });
-      }).catch(() => {
-        if(evt.request.url.indexOf('.html') > -1){
-          return caches.match('fallback.html');
-        } 
-      })
-    );
-  }
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener("fetch", function (event) {
+ 
 });
+
+function fromCache(request) {
+}
+
+function updateCache(request, response) {
+}
