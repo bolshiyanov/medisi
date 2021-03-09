0b6cff66dfbf7ecc20e4ecab7332623d44b3ecf4
bolshiyanov <bolshiyanov@gmail.com>
2021-03-09 12:52:14 +0000

    added new MongoDB

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,7 @@
 const config = {
     PORT: process.env.PORT || 3000,
-    MONGO_URL: "mongodb+srv://root:admin@cluster0.milmt.mongodb.net/Meditation-test?retryWrites=true&w=majority",
+    // MONGO_URL: "mongodb+srv://root:admin@cluster0.milmt.mongodb.net/Meditation-test?retryWrites=true&w=majority",
+    MONGO_URL: "mongodb+srv://Medisi:inulyasp123@cluster0.znivn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     secret_key: "sshkey"
 }
 
